0e914211fcd34400124a96065ea2f467f2d0cd8a
HOUND <45868404+Warrior-hound@users.noreply.github.com>
2024-03-20 12:24:31 +0530

    Update reader.ino

--- reader.ino
+++ reader.ino
@@ -1,4 +1,4 @@
- #include "HX710B.h"
+#include "HX711.h"
 int waterLevelSensorPin = A0; 
 const int DOUT = 4;
 const int SCLK = 5;  
@@ -9,17 +9,90 @@ int waterLevelReading;
 int pressureReading;
 bool pumpRunning = false;
 
-HX710B pressure_sensor(DOUT, SCLK);
+HX711 scale;
 
 void setup() {
-  pinMode(pumpPin, OUTPUT);
-  pinMode(solenoidValvePin, OUTPUT);
-  
   Serial.begin(9600);
 
-  if (!pressure_sensor.init()) {
-Serial.println("No pressure Sensor");
-}
+ pinMode(pumpPin, OUTPUT);
+  pinMode(solenoidValvePin, OUTPUT);
+  scale.begin(DOUT, SCLK);
+
+ Serial.println("Before setting up the scale:");
+
+  Serial.print("read: \t\t");
+
+  Serial.println(scale.read());                                  // print a raw reading from the ADC
+
+
+
+
+  Serial.print("read average: \t\t");
+
+  Serial.println(scale.read_average(20));              // print the average of 20 readings from the ADC
+
+
+
+
+  Serial.print("get value: \t\t");
+
+  Serial.println(scale.get_value(5));                        // print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+
+
+
+  Serial.print("get units: \t\t");
+
+  Serial.println(scale.get_units(5), 1);   // print the average of 5 readings from the ADC minus tare weight (not set) divided
+
+                                                                                                // by the SCALE parameter (not set yet)
+
+
+
+
+  scale.set_scale(2280.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+
+  scale.tare();                                                                   // reset the scale to 0
+
+
+
+
+  Serial.println("After setting up the scale:");
+
+
+
+
+  Serial.print("read: \t\t");
+
+  Serial.println(scale.read());                 // print a raw reading from the ADC
+
+
+
+
+  Serial.print("read average: \t\t");
+
+  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+
+
+
+  Serial.print("get value: \t\t");
+
+  Serial.println(scale.get_value(5));                        // print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+
+
+
+  Serial.print("get units: \t\t");
+
+  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
+
+                                                                                                // by the SCALE parameter set with set_scale
+
+
+
+
+  Serial.println("Readings:");
 }
 
 void loop() {
@@ -33,11 +106,18 @@ void loop() {
       pumpRunning = true;
     }
    
-    if (pressure_sensor.read(&rawData, 1000UL) != HX710B_OK) Serial.println("Error in Sensor");
-    else {
-    Serial.println("Pressure is");
-    Serial.println((unsigned long) rawData);
-  }
+     Serial.print("\t| average:\t");
+
+     Serial.println(scale.get_units(10), 1);
+
+
+
+
+     scale.power_down();                                                  // put the ADC in sleep mode
+
+    delay(5000);
+
+   scale.power_up();
     
 
     
@@ -62,9 +142,7 @@ void loop() {
   
   Serial.print("Water Level: ");
   Serial.print(waterLevelReading);
-  Serial.print("\t Pressure: ");
-  Serial.println((unsigned long) rawData);
-  
+
 
   
   delay(1000); 
